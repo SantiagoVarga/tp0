af4829f583c452f16e6f3b8f0476f07ad015e926
SantiagoVarga <santi.varga03@gmail.com>
2026-03-18 16:26:25 -0300

    ej2 pre-test

--- generar-compose.sh
+++ generar-compose.sh
@@ -12,45 +12,51 @@ if [[ -z "$OUTFILE" || -z "$NUM_CLIENTS" ]]; then
 	exit 1
 fi
 
+
 cat > "$OUTFILE" <<EOF
 name: tp0
 services:
-	server:
-		container_name: server
-		image: server:latest
-		entrypoint: python3 /main.py
-		environment:
-			- PYTHONUNBUFFERED=1
-			- LOGGING_LEVEL=DEBUG
-		networks:
-			- testing_net
+    server:
+        container_name: server
+        image: server:latest
+        entrypoint: python3 /main.py
+        environment:
+            - PYTHONUNBUFFERED=1
+            - LOGGING_LEVEL=DEBUG
+        networks:
+            - testing_net
+        volumes:
+            - ./server/config.ini:/config.ini
 EOF
+
 
 # Generar clientes
 for i in $(seq 1 "$NUM_CLIENTS"); do
 	cat >> "$OUTFILE" <<EOF
-	client$i:
-		container_name: client$i
-		image: client:latest
-		entrypoint: /client
-		environment:
-			- CLI_ID=$i
-			- CLI_LOG_LEVEL=DEBUG
-		networks:
-			- testing_net
-		depends_on:
-			- server
+    client$i:
+        container_name: client$i
+        image: client:latest
+        entrypoint: /client
+        environment:
+            - CLI_ID=$i
+            - CLI_LOG_LEVEL=DEBUG
+        networks:
+            - testing_net 
+        depends_on:
+            - server
+        volumes:
+            - ./client/config.yaml:/config.yaml
 EOF
 done
 
 # Definir redes
 cat >> "$OUTFILE" <<EOF
 networks:
-	testing_net:
-		ipam:
-			driver: default
-			config:
-				- subnet: 172.25.125.0/24
+    testing_net:
+        ipam:
+            driver: default
+            config:
+               - subnet: 172.25.125.0/24
 EOF
 
 echo "compose en $OUTFILE generado con $NUM_CLIENTS clientes."
