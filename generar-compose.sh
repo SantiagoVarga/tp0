b074c1a71f2cd0efb7fb363cc8337053205e6b81
SantiagoVarga <santi.varga03@gmail.com>
2026-03-22 17:46:32 -0300

    ej6 pathFix

--- generar-compose.sh
+++ generar-compose.sh
@@ -49,7 +49,7 @@ for i in $(seq 1 "$NUM_CLIENTS"); do
             - server
         volumes:
             - ./client/config.yaml:/config.yaml
-            - ./.data/agency-$i.csv:./data/agency-$i.csv
+            - ${PWD}/.data/dataset/agency-$i.csv:${PWD}/.data/dataset/agency-$i.csv
 EOF
 done
 
