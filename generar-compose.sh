a6e83985651c0ed65bcf2e554347ed8aac533bfd
SantiagoVarga <santi.varga03@gmail.com>
2026-03-22 17:59:06 -0300

    ej6 problems with yaml

--- generar-compose.sh
+++ generar-compose.sh
@@ -49,7 +49,7 @@ for i in $(seq 1 "$NUM_CLIENTS"); do
             - server
         volumes:
             - ./client/config.yaml:/config.yaml
-            - ${PWD}/.data/dataset/agency-$i.csv:${PWD}/.data/dataset/agency-$i.csv
+            - ${PWD}/.data/agency-$i.csv:${PWD}/.data/agency-$i.csv
 EOF
 done
 
