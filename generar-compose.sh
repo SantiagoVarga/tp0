c5e935c05c254a39ca721ea339a3ac045ba68686
SantiagoVarga <santi.varga03@gmail.com>
2026-03-22 18:11:32 -0300

    ej6 revision

--- generar-compose.sh
+++ generar-compose.sh
@@ -49,7 +49,7 @@ for i in $(seq 1 "$NUM_CLIENTS"); do
             - server
         volumes:
             - ./client/config.yaml:/config.yaml
-            - ${PWD}/.data/agency-$i.csv:${PWD}/.data/agency-$i.csv
+            - ./.data/agency-$i.csv:agency-$i.csv
 EOF
 done
 
