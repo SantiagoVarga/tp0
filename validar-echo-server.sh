11df6a0221332d849299ad699a3f37920a1516e7
SantiagoVarga <santi.varga03@gmail.com>
2026-03-24 19:35:43 -0300

    ej8 first clean

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Script para validar el funcionamiento del echo server usando netcat en un contenedor temporal
-# Uso: ./validar-echo-server.sh
+
 
 # Mensaje de prueba
 TEST_MSG="echo_test_$(date +%s)"
